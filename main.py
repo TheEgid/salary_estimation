223c6e45b7a2cec95d15a5cdc7325a23b3c4cdf5
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-01-16 15:11:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,20 @@ def print_ascitables(sys_command, languages):
     sj = 'SuperJob Moscow'
     all_commands = [hh, sj]
     valid_commands = ['hh', 'sj', 'all']
-
+    
+    sys_command = sys_command.strip()
     print('Getting {} salary statistics for {} ...'.format(sys_command,
                                                            ', '.join(
                                                                languages)))
-    if sys_command.strip() == valid_commands[0]:
+    if sys_command == valid_commands[0]:
         print_statistics_ascitables(hh,
                                     make_hh_salary_statistics(languages))
-    elif sys_command.strip() == valid_commands[1]:
+    elif sys_command == valid_commands[1]:
         print_statistics_ascitables(sj,
                                     make_sj_salary_statistics(secret_id,
                                                               secret_key,
                                                               languages))
-    elif sys_command.strip() == valid_commands[2]:
+    elif sys_command == valid_commands[2]:
         for command in all_commands:
             print_statistics_ascitables(command,
                                         make_hh_salary_statistics(languages))
@@ -40,6 +41,6 @@ if __name__ == '__main__':
     secret_id = os.getenv("id")
     advisable_languages = ['Java', 'PHP', 'С++', 'R', 'Python', 'JavaScript',
                            'Delphi', 'Go', '1C', 'Ruby']
-    sys_command = ''.join(sys.argv[1])
+    sys_command = sys.argv[1]
     print_ascitables(sys_command=sys_command, languages=advisable_languages)
 
