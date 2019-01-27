8335cd8c403d24696c01b7848a31111e907ddf34
TheEgid <40724661+TheEgid@users.noreply.github.com>
2019-01-27 21:17:35 +0300

    0.5

--- fetch_hh.py
+++ fetch_hh.py
@@ -1,35 +1,14 @@
 """Get HH Stat."""
 from __future__ import print_function
 import requests
-import urllib.parse
 from stat_services import predict_rub_salary
 from stat_services import make_salary_stat
 from stat_services import group_list
 
 
-class VacanciesDictionaryError(Exception):
-    """Declare special exception."""
-    pass
-
-
-def get_all_pages_vacancies_dict(language_list, search_period=30):
-    """
-    Get dictionaries with vacancies of programmers from Web API HeadHunter.ru.
-
-    Args:
-        language_list(list): The list with names of programming language.
-        search_period(int, optional): Over these last days search. Defaults to 30.
-    Returns:
-        all_pages_vacancies_dict(dict): Dictionary key is name of programming language.
-            Dictionary value contains list (2 elements). First element is first page url with 
-            search results vacancies for particular programming language. Second element is 
-            number of all pages (urls with search results).         
-    Raises:
-        ValueError: Raises an exception.
-    """
+def get_vacancies_pages_qty_list(language_list, search_period=30):
     _url = 'https://api.hh.ru/vacancies'
-    all_pages_vacancies_dict = {}
-    language_list.sort()
+    all_pages_vacancies_list = []
     for language in language_list:
         params = {
             'text': 'Программист {}'.format(language),
@@ -39,39 +18,26 @@ def get_all_pages_vacancies_dict(language_list, search_period=30):
         }
         response = requests.get(_url, params=params)
         if response.ok:
-            all_pages_vacancies_dict.update({language: [response.url, response.json()['pages']]})
+            all_pages_vacancies_list.append((language, response.json()['pages']))
         else:
             raise ValueError('response error!')
-    return all_pages_vacancies_dict
+    return all_pages_vacancies_list
 
 
-def make_page_vacancy_url_list(all_pages_vacancies_dict):
-    """
-    Get all pages with search results vacancies for particular programming language.
-
-    Args:
-        all_pages_vacancies_dict(dict): Dictionary key is name of programming
-        language. Dictionary value contains list (2 elements). First element is
-        first page url with search results vacancies for particular programming
-        language. Second element is number of all pages (urls with search results).
-    Returns:
-        new_vacancies(list): The list of dictionares with all pages url with 
-            search results vacancies for particular programming language
-    Raises:
-        VacanciesDictionaryError: Raises an exception KeyError, ValueError.
-    """
+def make_page_vacancy_url_list(qtyes, search_period=30):
+    lang, _pages_qty = qtyes
     new_vacancies = []
-    try:
-        for language, [_url, _pages_qty] in all_pages_vacancies_dict.items():
-            for page_number in range(0, _pages_qty):
-                query = urllib.parse.urlparse(_url).query
-                url_params = dict(urllib.parse.parse_qsl(query))
-                url_params.update({'page': page_number})
-                url = _url[:_url.find("?")]
-                new_vacancies.append({language: (url, url_params)})
-        return new_vacancies
-    except (KeyError, ValueError):
-        raise VacanciesDictionaryError
+    url = 'https://api.hh.ru/vacancies'
+    for page_number in range(0, _pages_qty):
+        params = {
+            'text': 'Программист {}'.format(lang),
+            'area': 1,
+            'from': 'cluster_area',
+            'period': search_period,
+            'page': page_number
+        }
+        new_vacancies.append({lang: (url, params)})
+    return new_vacancies
 
 
 def extract_language_and_salary(url_dict):
@@ -99,13 +65,18 @@ def extract_language_and_salary(url_dict):
 
 
 def make_hh_salary_statistics(language_list):
-    """Сollects statistics from hh.ru and display the average salary."""
-    searched_vacancies = get_all_pages_vacancies_dict(language_list)
-    all_vacancies_urls = make_page_vacancy_url_list(searched_vacancies)
-    temp_vacancy_language_list = \
-        [extract_language_and_salary(x) for x in all_vacancies_urls]
+    language_list.sort()
+    vacancies_pages_qty = get_vacancies_pages_qty_list(language_list)
+    all_vacancies_urls = [make_page_vacancy_url_list(x) for x in
+                          vacancies_pages_qty]
+    temp_vacancy_language_list = []
+    for lang in all_vacancies_urls:
+        for salary_dict in lang:
+            temp_vacancy_language_list.append(
+                extract_language_and_salary(salary_dict))
+
     vacancy_language_list = \
         group_list([vacancy for sublist in temp_vacancy_language_list
-                           for vacancy in sublist])
+                    for vacancy in sublist])
     return make_salary_stat(vacancy_language_list)
 
